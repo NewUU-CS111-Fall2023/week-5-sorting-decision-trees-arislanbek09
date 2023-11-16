20d8b9c961f835969726c0fdfa13416a96413969
Arislanbek Kalbaev <127713555+arislanbek09@users.noreply.github.com>
2023-11-16 17:13:17 +0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,93 @@
 /*
- * Author:
- * Date:
- * Name:
+ * Author: Arislanbek Kalbaev
+ * Date: November 16, 2023
+ * Name: Arislanbek Kalbaev
  */
 
 #include <iostream>
+#include <vector>
 #include "task_1.h"
+#include "task_2.h"
+#include "task_3.h"
+#include "task_4.h"
+#include "task_5.h"
+
+using namespace std;
 
 int main() {
-    std::cout << "Task 1" << std::endl;
-    // call for task 1
-    std::cout << "Task 2" << std::endl;
-    // call for task 2
-    std::cout << "Task 3" << std::endl;
-    // call for task 3
-    std::cout << "Task 4" << std::endl;
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
+    cout << "Task 1" << endl;
+
+    vector<string> books = {"The Catcher in the Rye", "To Kill a Mockingbird", "1984", "Pride and Prejudice", "The Great Gatsby"};
+
+    cout << "Unsorted List of Books:\n";
+    for (const auto& title : books) {
+        cout << title << '\n';
+    }
+
+    bubbleSort(books);
+    displaySortedBooks(books);
+
+
+
+
+
+    cout << "Task 2" << endl;
+
+    vector <int> denominations = {50, 10, 20, 5, 100, 1, 500};
+
+    cout << "Unsorted List of Denominations:\n";
+    for (int denomination : denominations) {
+        cout << denomination << ' ';
+    }
+    cout << '\n';
+
+    selectionSort(denominations);
+
+    displaySortedDenominations(denominations);
+
+
+
+
+
+    cout << "Task 3" << endl;
+
+    vector<int> soldiers = {1, 2, 3, 4, 5, 6, 7};
+
+    cout << "Original List of Soldiers:\n";
+    for (int soldier : soldiers) {
+        cout << soldier << ' ';
+    }
+    cout << '\n';
+
+    rearrangeSoldiers(soldiers);
+
+    displayRearrangedSoldiers(soldiers);
+
+
+
+
+
+    cout << "Task 4" << endl;
+
+    int arr[] = {4, 2, 8, 1, 6, 3, 9};
+
+    int result = countElementsWithSmallerNeighbors(arr, sizeof(arr) / sizeof(arr[0]));
+
+    cout << "Count of elements where both neighboring elements are smaller: " << result << std::endl;
+
+
+
+
+
+    cout << "Task 5" << endl;
+
+    vector<vector<char>> symbolMatrix = {
+            {'b', 'y', 'w'},
+            {'y', 'b', 'w'},
+            {'w', 'w', 'b'}
+    };
+
+    displayColorMatrix(symbolMatrix);
+
     return 0;
 }
